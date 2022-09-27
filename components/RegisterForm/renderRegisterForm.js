516e097fce8d2981dcc04b69754bb74c488a0f3e
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-09-27 19:54:56 +0200

    code added to renderRegisterForm

---
+++ components/RegisterForm/renderRegisterForm.js
@@ -0,0 +1,48 @@
+export default function () {
+	const section = document.querySelector('.content');
+	section.innerHTML = '';
+
+	const form = document.createElement('form');
+	form.setAttribute = ('id', 'register-form');
+
+	const h2 = document.createElement('h2');
+	h2.textContent = 'Register';
+
+	const inputEmail = document.createElement('input');
+	inputEmail.setAttribute = ('type', 'email');
+	inputEmail.setAttribute = ('placeholder', 'email');
+	inputEmail.setAttribute = ('id', 'input-email-register');
+
+	const inputPass1 = document.createElement('input');
+	inputPass1.setAttribute = ('type', 'password');
+	inputPass1.setAttribute = ('placeholder', 'password');
+	inputPass1.setAttribute = ('id', 'first-input-password-register');
+
+	const inputPass2 = document.createElement('input');
+	inputPass2.setAttribute = ('type', 'password');
+	inputPass2.setAttribute = ('placeholder', 'password');
+	inputPass2.setAttribute = ('id', 'second-input-password-register');
+
+	const button = document.createElement('button');
+	button.setAttribute = ('type', 'submit');
+	button.textContent = 'Register';
+
+	form.appendChild(h2);
+	form.appendChild(inputEmail);
+	form.appendChild(inputPass1);
+	form.appendChild(inputPass2);
+	form.appendChild(button);
+
+	section.appendChild(form);
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault()
+
+        const email = inputEmail.value
+        const password = inputPass1.value
+        const password2 = inputPass2.value
+
+    
+});
+
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // console.log(app);
 
 import renderHomePage from './components/HomePage/renderHomePage.js';
+import renderRegisterForm from './components/RegisterForm/renderRegisterForm.js';
 
 const homeButton = document.getElementById('home-anchor');
 const todosButton = document.getElementById('todos-anchor');
@@ -12,6 +13,8 @@ const loginButton = document.getElementById('login-anchor');
 
 //rendering the home page  on initial page load
 renderHomePage();
+renderRegisterForm();
 
 //rendering the home page on home button click
 homeButton.addEventListener('click', renderHomePage);
+loginButton.addEventListener('click', renderRegisterForm);
