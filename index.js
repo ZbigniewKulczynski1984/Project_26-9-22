2bfea4d64ced53383a4dad44440a6b76c75d3a63
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-09-28 21:06:29 +0200

    function logged out added

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 // console.log(app);
 
 import renderHomePage from './components/HomePage/renderHomePage.js';
-import renderRegisterForm from './components/RegisterForm/renderRegisterForm.js';
-import renderLoginForm from './components/LoginForm/renderLoginForm.js';
+// import renderRegisterForm from './components/RegisterForm/renderRegisterForm.js';
+// import renderLoginForm from './components/LoginForm/renderLoginForm.js';
 import renderLoginPage from './components/LoginPage/renderLoginPage.js';
+import {onAuthStateChanged, signOut,} from 'https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js';
+import { auth } from "./firebaseConfig.js";
 
 //selecting content container
 const contentContainer = document.querySelector('content');
@@ -15,6 +17,18 @@ const todosButton = document.getElementById('todos-anchor');
 const aboutButton = document.getElementById('about-anchor');
 const publicButton = document.getElementById('public-anchor');
 const loginButton = document.getElementById('login-anchor');
+
+//reagowanie na zalogowanie i wylogowanie
+onAuthStateChanged(auth, (user) => {
+	if (user) {
+		console.log(`User is logged in (${user.email}), onAuthStateChanged`);
+		loginButton.textContent = 'Log out';
+	} else {
+		console.log(`No user is logged in, onAuthStateChanged`);
+		loginButton.textContent = 'Log in';
+        renderHomePage();
+	}
+});
 
 //rendering the home page  on initial page load
 renderHomePage();
