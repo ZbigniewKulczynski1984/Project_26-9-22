f538f7babf5e15e195b271b8dbeb8bc970080383
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-09-28 20:10:46 +0200

    renderLoginPage code added

---
+++ components/LoginPage/renderLoginPage.js
@@ -0,0 +1,48 @@
+import renderLoginForm from '../LoginForm/renderLoginForm.js';
+import renderRegisterForm from '../RegisterForm/renderRegisterForm.js';
+import renderHomePage from '../HomePage/renderHomePage.js';
+import { auth } from '../../firebaseConfig.js';
+import { signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js';
+
+export default function () {
+	const contentContainer = document.querySelector('.content');
+	contentContainer.innerHTML = '';
+
+	const h2 = document.createElement('h2');
+	h2.textContent = 'Log in or sign up';
+
+	const p = document.createElement('p');
+	p.textContent =
+		'Our authentication mechanism uses Firebase Auth and is 100% secure.';
+
+	const label = document.createElement('label');
+	label.textContent = 'Log in';
+
+	const btn = document.createElement('button');
+	btn.setAttribute('id', 'register-button');
+	btn.textContent = 'Register';
+
+	contentContainer.appendChild(h2);
+	contentContainer.appendChild(p);
+	contentContainer.appendChild(label);
+	contentContainer.appendChild(loginForm);
+	contentContainer.appendChild(btn);
+
+	btn.addEventListener('click', () => {
+		contentContainer.innerHTML = '';
+		renderRegisterForm();
+	});
+
+	renderLoginForm.addEventListener('submit', () => {
+		event.preventDefault();
+		const email = document.getElementById('#input-email-login').value;
+		const password = document.getElementById('#input-password-login').value;
+
+		signInWithEmailAndPassword(auth, email, password).then(userCredentials);
+        
+	});
+}
+
+renderLoginForm();
+
+const loginForm = renderLoginForm();
--- index.js
+++ index.js
@@ -5,9 +5,10 @@
 import renderHomePage from './components/HomePage/renderHomePage.js';
 import renderRegisterForm from './components/RegisterForm/renderRegisterForm.js';
 import renderLoginForm from './components/LoginForm/renderLoginForm.js';
+import renderLoginPage from './components/LoginPage/renderLoginPage.js';
 
 //selecting content container
-const contentContainer = document.querySelector('content')
+const contentContainer = document.querySelector('content');
 
 const homeButton = document.getElementById('home-anchor');
 const todosButton = document.getElementById('todos-anchor');
@@ -17,9 +18,7 @@ const loginButton = document.getElementById('login-anchor');
 
 //rendering the home page  on initial page load
 renderHomePage();
-renderRegisterForm();
-renderLoginForm();
 
 //rendering the home page on home button click
 homeButton.addEventListener('click', renderHomePage);
-loginButton.addEventListener('click', console.log(123));
+loginButton.addEventListener('click', renderLoginPage);
