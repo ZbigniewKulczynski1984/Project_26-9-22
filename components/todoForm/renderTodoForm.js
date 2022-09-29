d14896b0af1a9d944f92dafec3c03d43fb6cde7e
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-09-29 19:17:14 +0200

    letter changed

--- components/todoForm/renderTodoForm.js
+++ components/todoForm/renderTodoForm.js
@@ -10,78 +10,75 @@ export default function () {
 	fieldset.setAttribute('id', 'todo-fieldset');
 
 	const legend = document.querySelector('legend');
-    legend.setAttribute('id', 'todo-legend')
-    legend.textContent = 'Select a category'
+	legend.setAttribute('id', 'todo-legend');
+	legend.textContent = 'Select a category';
 
-    const divWork = document.querySelector('div')
-    divWork.setAttribute('id', 'div-work')
+	const divWork = document.querySelector('div');
+	divWork.setAttribute('id', 'div-work');
 
-    const inputWork = document.querySelector('input')
-    inputWork.setAttribute('type', 'radio')
-    inputWork.setAttribute('id', 'radio-work')
-    inputWork.setAttribute('name', 'work')
-    inputWork.setAttribute('value', 'work')
+	const inputWork = document.querySelector('input');
+	inputWork.setAttribute('type', 'radio');
+	inputWork.setAttribute('id', 'radio-work');
+	inputWork.setAttribute('name', 'work');
+	inputWork.setAttribute('value', 'work');
 
-    const labelWork = document.querySelector('label')
-    labelWork.setAttribute('for', 'radio-work')
-    labelWork.textContent = 'Work'
+	const labelWork = document.querySelector('label');
+	labelWork.setAttribute('for', 'radio-work');
+	labelWork.textContent = 'Work';
 
+	const divLife = document.querySelector('div');
+	divLife.setAttribute('id', 'div-life');
 
-    const divLife = document.querySelector('div')
-    divLife.setAttribute('id', 'div-life')
+	const inputLife = document.querySelector('input');
+	inputLife.setAttribute('type', 'radio');
+	inputLife.setAttribute('id', 'radio-life');
+	inputLife.setAttribute('name', 'life');
+	inputLife.setAttribute('value', 'life');
 
-    const inputLife = document.querySelector('input')
-    inputLife.setAttribute('type', 'radio')
-    inputLife.setAttribute('id', 'radio-life')
-    inputLife.setAttribute('name', 'life')
-    inputLife.setAttribute('value', 'life')
+	const labelLife = document.querySelector('label');
+	labelLife.setAttribute('for', 'radio-life');
+	labelLife.textContent = 'Life';
 
-    const labelLife = document.querySelector('label')
-    labelLife.setAttribute('for', 'radio-life')
-    labelLife.textContent = 'Life'
+	const divSport = document.querySelector('div');
+	divSport.setAttribute('id', 'div-sport');
 
-    const divSport = document.querySelector('div')
-    divSport.setAttribute('id', 'div-sport')
+	const inputSport = document.querySelector('input');
+	inputSport.setAttribute('type', 'radio');
+	inputSport.setAttribute('id', 'radio-sport');
+	inputSport.setAttribute('name', 'sport');
+	inputSport.setAttribute('value', 'sport');
 
-    const inputSport = document.querySelector('input')
-    inputSport.setAttribute('type', 'radio')
-    inputSport.setAttribute('id', 'radio-sport')
-    inputSport.setAttribute('name', 'sport')
-    inputSport.setAttribute('value', 'sport')
+	const labelSport = document.querySelector('label');
+	labelSport.setAttribute('for', 'radio-sport');
+	labelSport.textContent = 'Sport';
 
-    const labelSport = document.querySelector('label')
-    labelSport.setAttribute('for', 'radio-sport')
-    labelSport.textContent = 'Sport'
+	const divEducation = document.querySelector('div');
+	divEducation.setAttribute('id', 'div-education');
 
-    const diveducation = document.querySelector('div')
-    divEducation.setAttribute('id', 'div-education')
+	const inputEducation = document.querySelector('input');
+	inputEducation.setAttribute('type', 'radio');
+	inputEducation.setAttribute('id', 'radio-education');
+	inputEducation.setAttribute('name', 'education');
+	inputEducation.setAttribute('value', 'education');
 
-    const inputEducation = document.querySelector('input')
-    inputEducation.setAttribute('type', 'radio')
-    inputEducation.setAttribute('id', 'radio-education')
-    inputEducation.setAttribute('name', 'education')
-    inputEducation.setAttribute('value', 'education')
+	const labelEducation = document.querySelector('label');
+	labelEducation.setAttribute('for', 'radio-education');
+	labelEducation.textContent = 'Education';
 
-    const labelEducation = document.querySelector('label')
-    labelEducation.setAttribute('for', 'radio-education')
-    labelEducation.textContent = 'Education'
+	fieldset.appendChild(legend);
+	fieldset.appendChild(divWork);
+	fieldset.appendChild(divLife);
+	fieldset.appendChild(divSport);
+	fieldset.appendChild(divEducation);
 
-    fieldset.appendChild(legend)
-    fieldset.appendChild(divWork)
-    fieldset.appendChild(divLife)
-    fieldset.appendChild(divSport)
-    fieldset.appendChild(divEducation)
-    
-    const submitBtn = document.querySelector('button')
-    submitBtn.setAttribute('type', 'submit')
-    submitBtn.setAttribute('id', 'todo-form-submit-button')
-    submitBtn.textContent = 'Add todo'
-    
-    form.appendChild(input)
-    form.appendChild(fieldset)
-    form.appendChild(submitBtn)
+	const submitBtn = document.querySelector('button');
+	submitBtn.setAttribute('type', 'submit');
+	submitBtn.setAttribute('id', 'todo-form-submit-button');
+	submitBtn.textContent = 'Add todo';
 
-       
+	form.appendChild(input);
+	form.appendChild(fieldset);
+	form.appendChild(submitBtn);
 }
 
-renderTodoForm()
+renderTodoForm();
