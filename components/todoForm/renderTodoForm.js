31b991c338732b9cee7f1eca7f8f18e096bf8565
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-09-29 19:52:09 +0200

    mistake removed

--- components/todoForm/renderTodoForm.js
+++ components/todoForm/renderTodoForm.js
@@ -1,67 +1,68 @@
 export default function () {
-	const form = document.querySelector('form');
+
+	const form = document.createElement('form');
 	form.setAttribute('id', 'todo-form');
 
-	const input = document.querySelector('input');
+	const input = document.createElement('input');
 	input.setAttribute('type', 'text');
 	input.setAttribute('id', 'todo-input');
 
-	const fieldset = document.querySelector('fieldset');
+	const fieldset = document.createElement('fieldset');
 	fieldset.setAttribute('id', 'todo-fieldset');
 
-	const legend = document.querySelector('legend');
+	const legend = document.createElement('legend');
 	legend.setAttribute('id', 'todo-legend');
 	legend.textContent = 'Select a category';
 
-	const divWork = document.querySelector('div');
+	const divWork = document.createElement('div');
 	divWork.setAttribute('id', 'div-work');
 
-	const inputWork = document.querySelector('input');
+	const inputWork = document.createElement('input');
 	inputWork.setAttribute('type', 'radio');
 	inputWork.setAttribute('id', 'radio-work');
 	inputWork.setAttribute('name', 'work');
 	inputWork.setAttribute('value', 'work');
 
-	const labelWork = document.querySelector('label');
+	const labelWork = document.createElement('label');
 	labelWork.setAttribute('for', 'radio-work');
 	labelWork.textContent = 'Work';
 
-	const divLife = document.querySelector('div');
+	const divLife = document.createElement('div');
 	divLife.setAttribute('id', 'div-life');
 
-	const inputLife = document.querySelector('input');
+	const inputLife = document.createElement('input');
 	inputLife.setAttribute('type', 'radio');
 	inputLife.setAttribute('id', 'radio-life');
 	inputLife.setAttribute('name', 'life');
 	inputLife.setAttribute('value', 'life');
 
-	const labelLife = document.querySelector('label');
+	const labelLife = document.createElement('label');
 	labelLife.setAttribute('for', 'radio-life');
 	labelLife.textContent = 'Life';
 
-	const divSport = document.querySelector('div');
+	const divSport = document.createElement('div');
 	divSport.setAttribute('id', 'div-sport');
 
-	const inputSport = document.querySelector('input');
+	const inputSport = document.createElement('input');
 	inputSport.setAttribute('type', 'radio');
 	inputSport.setAttribute('id', 'radio-sport');
 	inputSport.setAttribute('name', 'sport');
 	inputSport.setAttribute('value', 'sport');
 
-	const labelSport = document.querySelector('label');
+	const labelSport = document.createElement('label');
 	labelSport.setAttribute('for', 'radio-sport');
 	labelSport.textContent = 'Sport';
 
-	const divEducation = document.querySelector('div');
+	const divEducation = document.createElement('div');
 	divEducation.setAttribute('id', 'div-education');
 
-	const inputEducation = document.querySelector('input');
+	const inputEducation = document.createElement('input');
 	inputEducation.setAttribute('type', 'radio');
 	inputEducation.setAttribute('id', 'radio-education');
 	inputEducation.setAttribute('name', 'education');
 	inputEducation.setAttribute('value', 'education');
 
-	const labelEducation = document.querySelector('label');
+	const labelEducation = document.createElement('label');
 	labelEducation.setAttribute('for', 'radio-education');
 	labelEducation.textContent = 'Education';
 
@@ -71,7 +72,7 @@ export default function () {
 	fieldset.appendChild(divSport);
 	fieldset.appendChild(divEducation);
 
-	const submitBtn = document.querySelector('button');
+	const submitBtn = document.createElement('button');
 	submitBtn.setAttribute('type', 'submit');
 	submitBtn.setAttribute('id', 'todo-form-submit-button');
 	submitBtn.textContent = 'Add todo';
@@ -79,6 +80,8 @@ export default function () {
 	form.appendChild(input);
 	form.appendChild(fieldset);
 	form.appendChild(submitBtn);
+
+    return form;
 }
 
-renderTodoForm();
+// renderTodoForm();
