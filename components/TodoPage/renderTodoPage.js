03170f680d368c9bc1c977db592b095385d67351
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-10-05 18:25:14 +0200

    code added to renderTodoPage.js

--- components/TodoPage/renderTodoPage.js
+++ components/TodoPage/renderTodoPage.js
@@ -1,150 +1,144 @@
-import renderTodoForm from "../todoForm/renderTodoForm.js";
-import { app, auth, database } from "../../firebaseConfig.js";
+import renderTodoForm from '../todoForm/renderTodoForm.js';
+import { app, auth, database } from '../../firebaseConfig.js';
 import {
-  ref,
-  onValue,
-  push,
-  update,
-  remove,
-} from "https://www.gstatic.com/firebasejs/9.8.2/firebase-database.js";
+	ref,
+	onValue,
+	push,
+	update,
+	remove,
+} from 'https://www.gstatic.com/firebasejs/9.8.2/firebase-database.js';
 
 const renderH2 = () => {
-  const h2 = document.createElement("h2");
-  h2.textContent = "Add, remove and edit your todos";
-  return h2;
+	const h2 = document.createElement('h2');
+	h2.textContent = 'Add, remove and edit your todos';
+	return h2;
 };
 
 const appendElements = (rootEl, elsToAppend) => {
-  elsToAppend.forEach((el) => rootEl.appendChild(el));
+	elsToAppend.forEach((el) => rootEl.appendChild(el));
 };
 
 export default function () {
-  const contentContainer = document.querySelector(".content");
+	const contentContainer = document.querySelector('.content');
 
-  const todoRef = ref(database, "todos/" + auth.currentUser.uid);
-  const todoFormHandler = (event) => {
-    event.preventDefault();
-    const todoText = document.getElementById("todo-input").value;
-    const category = [...document.getElementsByName("category")].find(
-      (input) => input.checked
-    ).value;
-    push(todoRef, {
-      todoText,
-      category,
-    })
-      .then(() => console.log("Pushed the data successfully"))
-      .catch(() => console.log("Failed to push the data"));
-  };
-  onValue(todoRef, (snapshot) => {
-    const data = snapshot.val();
+	const todoRef = ref(database, 'todos/' + auth.currentUser.uid);
+	const todoFormHandler = (event) => {
+		event.preventDefault();
+		const todoText = document.getElementById('todo-input').value;
+		const category = [...document.getElementsByName('category')].find(
+			(input) => input.checked
+		).value;
+		push(todoRef, {
+			todoText,
+			category,
+		})
+			.then(() => console.log('Pushed the data successfully'))
+			.catch(() => console.log('Failed to push the data'));
+	};
+	onValue(todoRef, (snapshot) => {
+		const data = snapshot.val();
 
-    if (!data) {
-      
-      contentContainer.innerHTML = "";
+		if (!data) {
+			contentContainer.innerHTML = '';
 
-      
-      const h2 = renderH2();
-      
-      contentContainer.appendChild(h2);
+			const h2 = renderH2();
 
-      
-      const todoForm = renderTodoForm();
+			contentContainer.appendChild(h2);
 
-      
-      contentContainer.appendChild(todoForm);
+			const todoForm = renderTodoForm();
 
-      
-      todoForm.addEventListener("submit", todoFormHandler);
-    } else {
-      console.log(data);
-      
-      const todos = Object.values(data);
+			contentContainer.appendChild(todoForm);
 
-      
-      const h2 = renderH2();
+			todoForm.addEventListener('submit', todoFormHandler);
+		} else {
+			console.log(data);
 
-      
-      const listItems = todos.map((el, i) => {
-       
-        const li = document.createElement("li");
-        li.setAttribute("id", `li-${i}`);
+			const todos = Object.values(data);
 
-       
-        const div = document.createElement("div");
-        div.setAttribute("id", `div-${i}`);
+			const h2 = renderH2();
 
-       
-        const span = document.createElement("span");
-        span.textContent = `${el.todoText} (${el.category})`;
+			const listItems = todos.map((el, i) => {
+				const li = document.createElement('li');
+				li.setAttribute('id', `li-${i}`);
 
-       
-        const editButton = document.createElement("button");
-        editButton.setAttribute("id", `edit-button-${i}`);
-        editButton.setAttribute("class", "edit-button");
-        
-        editButton.textContent = "Edit";
+				const div = document.createElement('div');
+				div.setAttribute('id', `div-${i}`);
 
-       
-        const removeButton = document.createElement("button");
-        removeButton.setAttribute("id", `remove-button-${i}`);
-        removeButton.setAttribute("class", "remove-button");
-        removeButton.textContent = "Remove";
+				const span = document.createElement('span');
+				span.textContent = `${el.todoText} (${el.category})`;
 
-        
-        appendElements(div, [span, editButton, removeButton]);
+				const editButton = document.createElement('button');
+				editButton.setAttribute('id', `edit-button-${i}`);
+				editButton.setAttribute('class', 'edit-button');
 
-        
-        li.appendChild(div);
+				editButton.textContent = 'Edit';
 
-        
-        return li;
-      });
+				const removeButton = document.createElement('button');
+				removeButton.setAttribute('id', `remove-button-${i}`);
+				removeButton.setAttribute('class', 'remove-button');
+				removeButton.textContent = 'Remove';
 
-      
-      console.log(listItems);
+				appendElements(div, [span, editButton, removeButton]);
 
-      
-      const ul = document.createElement("ul");
-     
-      listItems.forEach((el) => ul.appendChild(el));
-      
-      contentContainer.innerHTML = "";
-      
-      appendElements(contentContainer, [h2, renderTodoForm(), ul]);
-      
-	  //----------------------------------------------
-      const todoForm = document.getElementById("todo-form");
-      
-      todoForm.addEventListener("submit", todoFormHandler);
-      
-      
-	  //------------------------------------------
+				li.appendChild(div);
 
-      const editButtons = [...document.getElementsByClassName("edit-button")];
-      editButtons.forEach((el, i) => {
-        el.addEventListener("click", function () {
-          this.remove();
-          const div = document.getElementById(`div-${i}`);
-          const todoForm = renderTodoForm();
-          todoForm.setAttribute("id", `todo-form-${i}`);
-          div.appendChild(todoForm);
-          todoForm.addEventListener("submit", function (event) {
-            event.preventDefault();
-            const todoText = this.childNodes[0].value;
-            console.log(todoText);
-            const category = [...this.getElementsByTagName("input")]
-              .slice(1, 5)
-              .find((input) => input.checked).value;
-            console.log(category);
-            const updates = {};
-            updates[`todos/${auth.currentUser.uid}/${Object.keys(data)[i]}`] = {
-              category,
-              todoText,
-            };
-            update(ref(database), updates);
-          });
-        });
-      });
-    }
-  });
+				return li;
+			});
+
+			console.log(listItems);
+
+			const ul = document.createElement('ul');
+
+			listItems.forEach((el) => ul.appendChild(el));
+
+			contentContainer.innerHTML = '';
+
+			appendElements(contentContainer, [h2, renderTodoForm(), ul]);
+
+			//----------------------------------------------
+			const todoForm = document.getElementById('todo-form');
+
+			todoForm.addEventListener('submit', todoFormHandler);
+
+			//------------------------------------------
+
+			const editButtons = [...document.getElementsByClassName('edit-button')];
+			editButtons.forEach((el, i) => {
+				el.addEventListener('click', function () {
+					this.remove();
+					const div = document.getElementById(`div-${i}`);
+					const todoForm = renderTodoForm();
+					todoForm.setAttribute('id', `todo-form-${i}`);
+					div.appendChild(todoForm);
+					todoForm.addEventListener('submit', function (event) {
+						event.preventDefault();
+						const todoText = this.childNodes[0].value;
+						console.log(todoText);
+						const category = [...this.getElementsByTagName('input')]
+							.slice(1, 5)
+							.find((input) => input.checked).value;
+						console.log(category);
+						const updates = {};
+						updates[`todos/${auth.currentUser.uid}/${Object.keys(data)[i]}`] = {
+							category,
+							todoText,
+						};
+						update(ref(database), updates);
+					});
+				});
+			});
+			//-----------------------------------------
+			const removeButtons = [
+				...document.getElementsByClassName("remove-button"),
+			  ];
+
+			  removeButtons.forEach((el, i) => {
+				el.addEventListener('click', function () {
+					this.parentElement.parentElement.remove();
+					remove(ref,`todos/${auth.currentUser.uid}/${Object.keys(data)[i]}`)
+				})
+			  })
+
+		}
+	});
 }
--- components/todoForm/renderTodoForm.js
+++ components/todoForm/renderTodoForm.js
@@ -1,98 +1,94 @@
 export default function () {
+	const form = document.createElement('form');
+	form.setAttribute('id', 'todo-form');
 
-const form = document.createElement("form");
-  form.setAttribute("id", "todo-form");
+	const todoInput = document.createElement('input');
+	todoInput.setAttribute('id', 'todo-input');
 
+	const fieldset = document.createElement('fieldset');
+	fieldset.setAttribute('id', 'todo-fieldset');
+
+	const legend = document.createElement('legend');
+	legend.textContent = 'Select a category';
+
+	const divWork = document.createElement('div');
+	divWork.setAttribute('id', 'div-work');
+
+	const radioWork = document.createElement('input');
+	radioWork.setAttribute('type', 'radio');
+	radioWork.setAttribute('id', 'radio-work');
+	radioWork.setAttribute('name', 'category');
+	radioWork.setAttribute('value', 'work');
+	radioWork.setAttribute('checked', 'true');
+
+	const workLabel = document.createElement('label');
+	workLabel.setAttribute('for', 'radio-work');
+	workLabel.textContent = 'Work';
+	divWork.appendChild(radioWork);
+	divWork.appendChild(workLabel);
+
+	const divLife = document.createElement('div');
+	divLife.setAttribute('id', 'div-life');
+
+	const radioLife = document.createElement('input');
+	radioLife.setAttribute('type', 'radio');
+	radioLife.setAttribute('id', 'radio-life');
+	radioLife.setAttribute('name', 'category');
+	radioLife.setAttribute('value', 'life');
+
+	const lifeLabel = document.createElement('label');
+	lifeLabel.setAttribute('for', 'radio-life');
+	lifeLabel.textContent = 'life';
+	divLife.appendChild(radioLife);
+	divLife.appendChild(lifeLabel);
+
+	const divSport = document.createElement('div');
+	divSport.setAttribute('id', 'div-Sport');
+
+	const radioSport = document.createElement('input');
+	radioSport.setAttribute('type', 'radio');
+	radioSport.setAttribute('id', 'radio-sport');
+	radioSport.setAttribute('name', 'category');
+	radioSport.setAttribute('value', 'sport');
+
+	const sportLabel = document.createElement('label');
+	sportLabel.setAttribute('for', 'radio-sport');
+	sportLabel.textContent = 'Sport';
+	divSport.appendChild(radioSport);
+	divSport.appendChild(sportLabel);
+
+	const divEducation = document.createElement('div');
+	divEducation.setAttribute('id', 'div-Education');
+
+	const radioEducation = document.createElement('input');
+	radioEducation.setAttribute('type', 'radio');
+	radioEducation.setAttribute('id', 'radio-education');
+	radioEducation.setAttribute('name', 'category');
+	radioEducation.setAttribute('value', 'education');
   
-  const todoInput = document.createElement("input");
-  todoInput.setAttribute("id", "todo-input");
+	const educationLabel = document.createElement('label');
+	educationLabel.setAttribute('for', 'radio-education');
+	educationLabel.textContent = 'Education';
+	divEducation.appendChild(radioEducation);
+	divEducation.appendChild(educationLabel);
 
-  
-  const fieldset = document.createElement("fieldset");
-  fieldset.setAttribute("id", "todo-fieldset");
+	const submitButton = document.createElement('button');
+	submitButton.setAttribute('type', 'submit');
+	submitButton.setAttribute('class', 'todo-form-submit-button');
+	submitButton.textContent = 'Add todo';
 
-  
-  const legend = document.createElement("legend");
-  legend.textContent = "Select a category";
+	fieldset.appendChild(legend);
+	fieldset.appendChild(divWork);
+	fieldset.appendChild(divLife);
+	fieldset.appendChild(divSport);
+	fieldset.appendChild(divEducation);
 
-  
-  const divWork = document.createElement("div");
-  divWork.setAttribute("id", "div-work");
-  const radioWork = document.createElement("input");
-  radioWork.setAttribute("type", "radio");
-  radioWork.setAttribute("id", "radio-work");
-  radioWork.setAttribute("name", "category");
-  radioWork.setAttribute("value", "work");
-  radioWork.setAttribute("checked", "true");
-  const workLabel = document.createElement("label");
-  workLabel.setAttribute("for", "radio-work");
-  workLabel.textContent = "Work";
-  divWork.appendChild(radioWork);
-  divWork.appendChild(workLabel);
+	form.appendChild(todoInput);
+	form.appendChild(fieldset);
+	form.appendChild(submitButton);
 
-  const divLife = document.createElement("div");
-  divLife.setAttribute("id", "div-life");
-  const radioLife = document.createElement("input");
-  radioLife.setAttribute("type", "radio");
-  radioLife.setAttribute("id", "radio-life");
-  radioLife.setAttribute("name", "category");
-  radioLife.setAttribute("value", "life");
-  const lifeLabel = document.createElement("label");
-  lifeLabel.setAttribute("for", "radio-life");
-  lifeLabel.textContent = "life";
-  divLife.appendChild(radioLife);
-  divLife.appendChild(lifeLabel);
-
-  const divSport = document.createElement("div");
-  divSport.setAttribute("id", "div-Sport");
-  const radioSport = document.createElement("input");
-  radioSport.setAttribute("type", "radio");
-  radioSport.setAttribute("id", "radio-sport");
-  radioSport.setAttribute("name", "category");
-  radioSport.setAttribute("value", "sport");
-  const sportLabel = document.createElement("label");
-  sportLabel.setAttribute("for", "radio-sport");
-  sportLabel.textContent = "Sport";
-  divSport.appendChild(radioSport);
-  divSport.appendChild(sportLabel);
-
-  const divEducation = document.createElement("div");
-  divEducation.setAttribute("id", "div-Education");
-  const radioEducation = document.createElement("input");
-  radioEducation.setAttribute("type", "radio");
-  radioEducation.setAttribute("id", "radio-education");
-  radioEducation.setAttribute("name", "category");
-  radioEducation.setAttribute("value", "education");
-  const educationLabel = document.createElement("label");
-  educationLabel.setAttribute("for", "radio-education");
-  educationLabel.textContent = "Education";
-  divEducation.appendChild(radioEducation);
-  divEducation.appendChild(educationLabel);
-
-  
-  const submitButton = document.createElement("button");
-  submitButton.setAttribute("type", "submit");
-  submitButton.setAttribute("class", "todo-form-submit-button");
-  submitButton.textContent = "Add todo";
-
-  
-  fieldset.appendChild(legend);
-  fieldset.appendChild(divWork);
-  fieldset.appendChild(divLife);
-  fieldset.appendChild(divSport);
-  fieldset.appendChild(divEducation);
-
-  
-  form.appendChild(todoInput);
-  form.appendChild(fieldset);
-  form.appendChild(submitButton);
-
-  
-  return form;
+	return form;
 }
-
-
-
 
 // export default function () {
 // 	const form = document.createElement('form');
@@ -180,9 +176,6 @@ const form = document.createElement("form");
 
 // 	return form;
 // }
-
-
-
 
 //alternatywna opcja
 // const categories = ["work", "life", "sport", "education"];
